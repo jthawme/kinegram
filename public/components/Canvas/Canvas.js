ce43ac23c54b685c5ad4396efb29dc5534ddee76
Jonathan Thaw <hi@jthaw.me>
2019-01-09 22:11:32 +0000

    Added some controls and mainly control from the outside

--- public/components/Canvas/Canvas.js
+++ public/components/Canvas/Canvas.js
@@ -11,20 +11,18 @@ import classNames from 'classnames';
 // CSS, Requires
 import "./Canvas.scss";
 import { imageLoaderManager, imageLoader } from '../../utils/imageLoader';
-const frames = [
-  require('../../images/testframes/02/01.png'),
-  require('../../images/testframes/02/02.png'),
-  require('../../images/testframes/02/03.png'),
-  require('../../images/testframes/02/04.png'),
-  require('../../images/testframes/02/05.png'),
-  require('../../images/testframes/02/06.png'),
-  require('../../images/testframes/02/07.png'),
-  require('../../images/testframes/02/08.png'),
-];
 
 class Canvas extends React.Component {
   static propTypes = {
     className: PropTypes.string
+  };
+
+  static defaultProps = {
+    lineWidth: 1,
+    frameRate: 5,
+    color: 'black',
+    withBars: true,
+    onLoading: () => {}
   };
 
   constructor(props) {
@@ -32,17 +30,54 @@ class Canvas extends React.Component {
 
     this.state = {
       size: 400,
-      frames: frames.length,
+      frames: frames,
       lineWid: 1,
       frameRate: 5,
-      color: 'red'
+      color: 'red',
+      withBars: true,
+      playing: false
     };
 
+    this.initialLoad = false;
     this.frame = 0;
     this.counter = 0;
   }
 
-  shouldComponentUpdate() {
+  static getDerivedStateFromProps(props, state) {
+    return {
+      frames: props.frames,
+      lineWid: props.lineWidth,
+      frameRate: props.frameRate,
+      color: props.color,
+      withBars: props.withBars,
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextState.loading !== this.state.loading);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.loading !== this.state.loading) {
+      this.props.onLoading(this.state.loading);
+    }
+
+    if (this.gotNewFrames(prevProps.frames, this.props.frames) && this.initialLoad) {
+      this.reset();
+    }
+  }
+
+  gotNewFrames(prevFrames, frames) {
+    if (prevFrames.length !== frames.length) {
+      return true;
+    }
+
+    for (let i = 0; i < prevFrames.length; i++) {
+      if (prevFrames[i] !== frames[i]) {
+        return true;
+      }
+    }
+
     return false;
   }
 
@@ -53,11 +88,51 @@ class Canvas extends React.Component {
       this.ctx = this.canvasRef.getContext('2d');
       this.setDimensions(this.state.size);
 
-      Promise.all([
-        imageLoaderManager(frames)
-      ])
-        .then(this.create);
+      this.initialLoad = true;
+      this.reset();
     }
+  }
+  
+  reset() {
+    this.loadEngine(this.state.frames)
+      .then(() => {
+        this.loop();
+      });
+  }
+
+  loadEngine = (frames) => {
+    return new Promise((resolve, reject) => {
+      this.setState({
+        loading: true,
+        playing: false
+      }, () => {
+        Promise.all([
+          this.createFrames(frames),
+          this.delayedTimer()
+        ])
+          .then(() => {
+            this.setState({
+              loading: false,
+              playing: true
+            }, () => {
+              resolve();
+            });
+          });
+      });
+    });
+  }
+
+  delayedTimer() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+  }
+
+  createFrames(frames) {
+    return imageLoaderManager(frames)
+      .then(this.create);
   }
 
   setDimensions = (size) => {
@@ -69,42 +144,42 @@ class Canvas extends React.Component {
     this.ctx.scale(dpr, dpr);
   }
 
-  create = ([images]) => {
+  create = (images) => {
     const { size } = this.state;
 
     this.ctx.clearRect(0, 0, size, size);
     this.bars = this.getBars();
-
-    Promise.all(
+    this.ctx.fill(this.bars);
+    
+    return Promise.all(
       images.map((i, index) => {
         return this.getFrame(i, index);
       })
     )
       .then(frames => {
         this.frameImages = frames;
-
-        this.loop();
+        return frames;
       });
   }
 
   draw = () => {
-    const { size, lineWid, frames, color } = this.state;
-    const block = (frames * lineWid) + lineWid;
+    const { size, lineWid, frames, color, withBars } = this.state;
+    const block = (frames.length * lineWid) + lineWid;
     const dpr = window.devicePixelRatio;
 
     this.ctx.clearRect(0, 0, size, size);
     
     this.frameImages.forEach((f, index) => {
-      // if (index < 2) {
-        this.ctx.drawImage(f, 0, 0, f.width / dpr, f.height / dpr);
-      // }
+      this.ctx.drawImage(f, 0, 0, f.width / dpr, f.height / dpr);
     });
     this.ctx.restore();
 
-    this.ctx.save();
-    this.ctx.translate(((this.counter % frames) * lineWid) - block, 0);
-    this.ctx.fill(this.bars);
-    this.ctx.restore();
+    if (withBars) {
+      this.ctx.save();
+      this.ctx.translate(((this.counter % frames.length) * lineWid) - block, 0);
+      this.ctx.fill(this.bars);
+      this.ctx.restore();
+    }
 
     this.ctx.save();
     this.ctx.globalCompositeOperation = 'source-atop';
@@ -114,17 +189,19 @@ class Canvas extends React.Component {
   }
 
   loop = () => {
-    if (this.frame % this.state.frameRate === 0) {
-      this.draw();
-      this.counter++;
-    }
-    this.frame++;
+    if (this.state.playing) {
+      if (this.frame % this.state.frameRate === 0) {
+        this.draw();
+        this.counter++;
+      }
+      this.frame++;
 
-    requestAnimationFrame(() => this.loop());
+      requestAnimationFrame(() => this.loop());
+    }
   }
 
   getFrame(image, num) {
-    const { size, color } = this.state;
+    const { size, color, lineWid } = this.state;
 
     this.ctx.save();
     this.ctx.clearRect(0, 0, size, size);
@@ -133,7 +210,7 @@ class Canvas extends React.Component {
 
     this.ctx.globalCompositeOperation = 'destination-out';
 
-    this.ctx.translate(num, 0);
+    this.ctx.translate(num * lineWid, 0);
     this.ctx.fill(this.bars);
     this.ctx.restore();
 
@@ -142,8 +219,8 @@ class Canvas extends React.Component {
 
   getBars() {
     const { size, frames, lineWid } = this.state;
-    const block = (frames * lineWid) + lineWid;
-
+    const block = (frames.length * lineWid) + lineWid;
+    
     const shape = new Path2D();
     for (let i = 0; i < (size + block); i += block) {
       shape.rect(i + lineWid, 0, block - lineWid, size);
@@ -154,14 +231,17 @@ class Canvas extends React.Component {
 
   render() {
     const { className } = this.props;
-    const { size } = this.state;
+    const { size, loading } = this.state;
 
     const cls = classNames(
       className,
-      'canvas'
+      'canvas',
+      {
+        'canvas--hide': loading
+      }
     );
 
-    return <canvas ref={this.getRef} width={`${size}px`} height={`${size}px`}/>;
+    return <canvas className={cls} ref={this.getRef} width={`${size}px`} height={`${size}px`}/>;
   }
 }
 
--- public/components/Home/Home.js
+++ public/components/Home/Home.js
@@ -11,6 +11,25 @@ import { bindActionCreators } from 'redux';
 // Components
 import Canvas from '../Canvas/Canvas';
 
+// const frames = [
+//   require('../../images/testframes/01.png'),
+//   require('../../images/testframes/02.png'),
+//   require('../../images/testframes/03.png'),
+//   require('../../images/testframes/04.png'),
+//   require('../../images/testframes/05.png')
+// ];
+
+const frames = [
+  require('../../images/testframes/02/01.png'),
+  require('../../images/testframes/02/02.png'),
+  require('../../images/testframes/02/03.png'),
+  require('../../images/testframes/02/04.png'),
+  require('../../images/testframes/02/05.png'),
+  require('../../images/testframes/02/06.png'),
+  require('../../images/testframes/02/07.png'),
+  require('../../images/testframes/02/08.png'),
+];
+
 // CSS, Requires
 import "./Home.scss";
 
@@ -18,14 +37,90 @@ class Home extends React.Component {
   static propTypes = {
   };
 
+  state = {
+    withBars: true,
+    frameRate: 5,
+    color: '#000000',
+    canvasLoading: true
+  };
+
+  onCheckboxChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.checked
+    });
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onRadioChange = (e) => {
+    this.setState({
+      [e.target.name]: parseInt(e.target.value, 10)
+    });
+  }
+
+  setCanvasLoading = (loading) => {
+    this.setState({
+      canvasLoading: loading
+    });
+  }
+
   render() {
+    const { withBars, color, frameRate, canvasLoading } = this.state;
     const cls = classNames(
-      'home'
+      'home',
+      {
+        'home--canvasLoading': canvasLoading
+      }
     );
+
+    const frameRates = [3, 5, 10];
 
     return (
       <div className={cls}>
-        <Canvas/>
+        <aside className="home__controls">
+          <ul>
+            <li>
+              <label className="home__controls__row">
+                <span className="home__controls__row__title">With Bars</span>
+                <span className="home__controls__row__input">
+                  <input type="checkbox" name="withBars" onChange={this.onCheckboxChange} value={1} checked={withBars}/>
+                </span>
+              </label>
+            </li>
+            <li>
+              <label className="home__controls__row">
+                <span className="home__controls__row__title">Color</span>
+                <span className="home__controls__row__input">
+                  <input type="color" name="color" onChange={this.onValueChange} value={color}/>
+                </span>
+              </label>
+            </li>
+            <li>
+              <label className="home__controls__row">
+                <span className="home__controls__row__title">Color</span>
+                <span className="home__controls__row__input">
+                  {
+                    frameRates.map(fr => {
+                      return <input key={fr} type="radio" name="frameRate" onChange={this.onRadioChange} value={fr} checked={frameRate === fr}/>
+                    })
+                  }
+                </span>
+              </label>
+            </li>
+          </ul>
+        </aside>
+        <div className="home__canvas">
+          <Canvas
+            frames={frames}
+            withBars={withBars}
+            color={color}
+            frameRate={frameRate}
+            onLoading={this.setCanvasLoading}/>
+        </div>
       </div>
     );
   }
